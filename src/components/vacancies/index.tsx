8e1e19eb01705c72a3018f99a6a383121ece10d3
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-10-03 11:47:23 +0100

    updated the slider component

--- src/components/vacancies/index.tsx
+++ src/components/vacancies/index.tsx
@@ -1,20 +1,23 @@
-import { VacancySlider } from "../vacanyslider"
+import { VacancySlider } from "../vacanyslider";
 
 export const Vacanies = () => {
-    return (
-        <div className="bg-[#dddddd]">
-            <div className="flex flex-col text-center p-5">
+  return (
+    <div className="bg-[#dddddd]">
+      <div className="flex flex-col text-center pt-10 p-5">
+        <p className="font-semibold  capitalize  text-3xl md:text-[3rem] text-gray-800 mb-6 ">
+          Come join us
+        </p>
+        <span className=" text-xl text-gray-500">
+          and make an impact in Indonesia
+        </span>
 
-                <p className="font-semibold text-[3rem] text-gray-800 ">Come join us</p>
-                <span className="text-[3rem]">and make an impact in Indonesia</span>
-
-            <div>
-            <button className="rounded-full p-4 text-gray-100 bg-[#038767] p-[1rem] w-[220px] mt-[2rem]">
-                Search for vacancies
-            </button>
-            </div>
-            </div>
-           <VacancySlider/>
+        <div>
+          <button className="rounded-full  text-gray-100 bg-[#038767] p-[1rem] w-[220px] mt-[2rem]">
+            Search for vacancies
+          </button>
         </div>
-    )
-}
+      </div>
+      <VacancySlider />
+    </div>
+  );
+};
--- src/components/vacanyslider/index.tsx
+++ src/components/vacanyslider/index.tsx
@@ -1,53 +1,42 @@
-import { useState } from "react";
 import { vacancySliderData } from "../../data/slider";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import { Container } from "../skeleton/container";
 
 export const VacancySlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const goToPreviousSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? vacancySliderData.length - 1 : prevIndex - 1
-    );
-  };
-
-  const goToNextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === vacancySliderData.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-
-  const imageWidth = 24.5; 
-
   return (
-    <div className="no-scroll-bar scroll-smooth">
-      <div className="flex gap-12 p-10 mb-10" style={{ transform: `translateX(-${currentIndex * imageWidth}%)` }}>
-        {vacancySliderData.concat(vacancySliderData).map((data, index) => (
-          <img
-            key={index}
-            src={data.src}
-            alt={data.alt}
-            style={{
-              width: `${imageWidth}%`, 
-            }}
-          />
-        ))}
-      </div>
-      <div className="flex justify-between p-3">
-        <FaChevronLeft
-          size="50"
-          color="#038767"
-          onClick={goToPreviousSlide}
-          className="cursor-pointer"
-        />
-        <FaChevronRight
-          size="50"
-          color="#038767"
-          onClick={goToNextSlide}
-          className="cursor-pointer"
-        />
-      </div>
+    <div className="py-20">
+      <Container>
+        <Swiper
+          slidesPerView={5}
+          centeredSlides={true}
+          spaceBetween={30}
+          pagination={{
+            type: "fraction",
+          }}
+          navigation={true}
+          autoplay
+          loop
+          modules={[Pagination, Navigation, Autoplay]}
+          className="mySwiper  "
+        >
+          {[...vacancySliderData, ...vacancySliderData].map((item) => (
+            <SwiperSlide
+              key={item.id}
+              className="!h-[15rem] !w-[15rem] overflow-hidden"
+            >
+              <img
+                src={item.src}
+                alt={item.alt}
+                className="w-full h-full object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Container>
     </div>
   );
 };
